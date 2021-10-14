0545f31d270d212d8fabb6da194072c10dddcd2b
Saladghri <439201085@student.ksu.edu.sa>
2021-10-14 22:48:26 +0300

    Design table of joining requests

--- db_connecting.php
+++ db_connecting.php
@@ -2,7 +2,7 @@
 $servername = 'localhost';
 $username = 'root';
 $password = 'root';
-$dbname = 'AwonDB';
+$dbname = 'awondb';
 $conn=new mysqli($servername ,$username ,$password ,$dbname );
 if(!$conn)
 {
