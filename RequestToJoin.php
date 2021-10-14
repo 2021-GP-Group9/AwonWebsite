e4f43d631ed40e8ce7c19aa79fdee6346e478fb9
Saladghri <439201085@student.ksu.edu.sa>
2021-10-14 17:56:07 +0300

    rename some files

---
+++ RequestToJoin.php
@@ -0,0 +1,80 @@
+<html lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel='stylesheet' href='style.css'>
+    <header id="headerPage" style="padding:128px 16px">
+            <form id="signout" action="logout.php" method="POST">
+                <input type="submit" value="تسجيل خروج">
+            </form> 
+            <img src="logo.jpg" alt="logo" class="pageP"  >
+        </header>
+    <body>
+        
+        <div class="auth-content"> 
+            <?php
+            echo '<h1>نموذج تقديم المنظمات الخيرية</h1>';
+            ?>      
+            <form method="post" id="JoiningRequestForm" >
+
+
+                <h2> <br>التسجيل</h2>
+                <br>
+                <br>
+
+                <input type="text" name="username" id="username" class="name-input" placeholder="اسم المنظمة الخيرية" required>
+                <input type="text" name="username" id="username" class="name-input" placeholder="اسم المستخدم "required>
+                <input type="text" name="username" id="username" class="name-input" placeholder="وصف المنظمة الخيرية" required>
+                <br><br>
+                <input type="email" name="username" id="username" class="name-input" placeholder="البريد الالكتروني "required>
+                <input type="password" name="pwd" class="password" id="password" placeholder="كلمة المرور" required >
+                <br><br>
+
+                <input type="number" name="username" id="username" class="name-input" placeholder="رقم الجوال"required><!-- tel or number?  -->
+                <input type="number" name="username" id="username" class="name-input" placeholder="رقم الترخيص"required>
+                <input type="image" name="username" id="username" class="name-input" placeholder="صورة الملف التعريفي"required><!-- it is uploade not sure -->
+                <br><br>
+                <input type="url" name="username" id="username" class="name-input" placeholder="الموقع"required><!-- not sure if it is url maybe it is select -->
+               <label>هل تتوفر خدمة التوصيل ؟ </label>
+                  <input type="radio" name="username" id="username" class="name-input">
+                  <label for="نعم">نعم</label>
+                  <input type="radio" name="username" id="username" class="name-input">
+                  <label for="لا">لا</label>
+                <br><br>
+                <label>انواع التبرع التي تقبل به المنظمة الخيرية؟</label>
+                <input type="checkbox" name="username" id="username" class="name-input">
+                <label for="ملابس">ملابس</label>
+                <input type="checkbox" name="username" id="username" class="name-input">
+                <label for="اثاث">اثاث</label>
+                <input type="checkbox" name="username" id="username" class="name-input">
+                <label for="الكترونيات">الكترونيات</label>
+                <br>
+
+
+                <input type="submit" class="bu1" value="تسجيل"/>
+
+                <br><br>
+
+
+
+                </div>
+
+
+            
+
+
+                </body>
+    <!-- Footer -->
+                <footer class="footer">  
+                    <div class="SOCIAL">
+                        <br>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                    </div>
+                    <p>&copy; KSU|Desigend by Aljawharah, Lamya, Rahaf, Sahar and Leen</p>
+                </footer>
+
+
+                </html>
+
--- joiningRequests.php
+++ joiningRequests.php
@@ -1,80 +1,47 @@
-<html lang="en">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel='stylesheet' href='style.css'>
-    <header id="headerPage" style="padding:128px 16px">
-            <form id="signout" action="logout.php" method="POST">
-                <input type="submit" value="تسجيل خروج">
-            </form> 
-            <img src="logo.jpg" alt="logo" class="pageP"  >
-        </header>
-    <body>
-        
+<html lang="en"><head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+        <title>manage joining request</title>
+    </head> 
+    <body data-new-gr-c-s-loaded="9.38.0"><header id="headerPage" style="padding:128px 16px">
+        <img src="logo.jpg" alt="logo" class="pageP">
+    </header>
+    
+
         <div class="auth-content"> 
-            <?php
-            echo '<h1>نموذج تقديم المنظمات الخيرية</h1>';
-            ?>      
-            <form method="post" id="JoiningRequestForm" >
+            <h1>طلبات الاضافة</h1>      
+            <table id="manageJoiningRequest">
+                <tbody><tr>
+                    <th>قبول /رفض</th>
+                    <th>اسم المنظمة الخيرية </th>
+                    <th>صورة المنظمة الخيرية</th>
+                </tr><tr>  
+                    <td><input type="button" value="قبول" onclick="">
+                        <input type="button" value="رفض" onclick=""></td>
+                    <td><a href="" id="">اسم المنظمة الخيرية</a></td>
+                    <td><img src="pic_trulli.jpg" alt="صورة المنظمة الخيرية"></td>
+
+                </tr>
+            </tbody></table>
+
+        </div>
 
 
-                <h2> <br>التسجيل</h2>
-                <br>
-                <br>
-
-                <input type="text" name="username" id="username" class="name-input" placeholder="اسم المنظمة الخيرية" required>
-                <input type="text" name="username" id="username" class="name-input" placeholder="اسم المستخدم "required>
-                <input type="text" name="username" id="username" class="name-input" placeholder="وصف المنظمة الخيرية" required>
-                <br><br>
-                <input type="email" name="username" id="username" class="name-input" placeholder="البريد الالكتروني "required>
-                <input type="password" name="pwd" class="password" id="password" placeholder="كلمة المرور" required >
-                <br><br>
-
-                <input type="number" name="username" id="username" class="name-input" placeholder="رقم الجوال"required><!-- tel or number?  -->
-                <input type="number" name="username" id="username" class="name-input" placeholder="رقم الترخيص"required>
-                <input type="image" name="username" id="username" class="name-input" placeholder="صورة الملف التعريفي"required><!-- it is uploade not sure -->
-                <br><br>
-                <input type="url" name="username" id="username" class="name-input" placeholder="الموقع"required><!-- not sure if it is url maybe it is select -->
-               <label>هل تتوفر خدمة التوصيل ؟ </label>
-                  <input type="radio" name="username" id="username" class="name-input">
-                  <label for="نعم">نعم</label>
-                  <input type="radio" name="username" id="username" class="name-input">
-                  <label for="لا">لا</label>
-                <br><br>
-                <label>انواع التبرع التي تقبل به المنظمة الخيرية؟</label>
-                <input type="checkbox" name="username" id="username" class="name-input">
-                <label for="ملابس">ملابس</label>
-                <input type="checkbox" name="username" id="username" class="name-input">
-                <label for="اثاث">اثاث</label>
-                <input type="checkbox" name="username" id="username" class="name-input">
-                <label for="الكترونيات">الكترونيات</label>
-                <br>
-
-
-                <input type="submit" class="bu1" value="تسجيل"/>
-
-                <br><br>
-
-
-
-                </div>
-
-
-            
-
-
-                </body>
+    
     <!-- Footer -->
-                <footer class="footer">  
-                    <div class="SOCIAL">
-                        <br>
-                        <a href="#"><i class="fab fa-twitter"></i></a>
-                        <a href="#"><i class="fab fa-instagram"></i></a>
-                        <a href="#"><i class="fab fa-youtube"></i></a>
-                        <a href="#"><i class="fab fa-facebook"></i></a>
-                    </div>
-                    <p>&copy; KSU|Desigend by Aljawharah, Lamya, Rahaf, Sahar and Leen</p>
-                </footer>
+    <footer class="footer">  
+        <div class="SOCIAL">
+            <br>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+            <a href="#"><i class="fab fa-facebook"></i></a>
+        </div>
+        <p>© KSU|Desigend by Aljawharah, Lamya, Rahaf, Sahar and Leen</p>
+    </footer>
 
 
-                </html>
 
+
+</body><grammarly-desktop-integration data-grammarly-shadow-root="true"></grammarly-desktop-integration></html>
