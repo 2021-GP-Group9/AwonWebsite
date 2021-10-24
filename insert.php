96ef12b7f6d44b681fa856a0eeb5c6c8a63a16c2
AljawharahDS <90795422+AljawharahDS@users.noreply.github.com>
2021-10-24 23:53:43 +0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -12,36 +12,71 @@ session_start();
 if (isset($_SESSION['role'])) {
     if ($_SESSION['role'] == 'admin') {
         ?>
+        <script>
+            //this for alert 
+        //Swal.fire({
+        //  title: 'هل تريد الاستمرار؟',
+        //  icon: 'question',
+        //  iconHtml: '؟',
+        //  confirmButtonText: 'نعم',
+        //  cancelButtonText: 'لا',
+        //  showCancelButton: true,
+        //  showCloseButton: true
+        //})
+        //    
+        </script>
         <?php
         // put your code here
         $connection = mysqli_connect("localhost", "root", "root", "awondb");
-        
-        $id = $_GET['ID'];
+//        $error = mysqli_connect_error();
+//        if ($error != null) {
+//            echo "<p>Eror!! could not connect to DB may not connect </p>";
+//        } else {
+//            echo 'success connect ';
+//        }
 
-        $sqli = "UPDATE charity SET status='Accepted' WHERE ID ='".$id."' ";
-        
-        mysqli_query($connection, $sqli);
-        if (mysqli_query($connection, $sqli)) {
+
+        $id = $_GET['id'];
+//        echo $id;
+        $sqli = "SELECT * FROM `charity` WHERE ID='$id' ";
+//        $result = $connection->query($sqli);
+//        while ($row = $result->fetch_assoc()) {
+//            echo "<p>" . $row['email'] . "</p>";
+//            echo "<p>" . $row['status'] . "</p>";
+//        }
+        $status = $_GET['status'];
+
+        $sqli2 = "UPDATE charity SET status='Accepted' WHERE ID ='$id'";
+
+//        $result = $connection->query($sqli2);
+//        while ($row = $result->fetch_assoc()) {
+//            echo "<p>" . $row['email'] . "</p>";
+//            echo "<p>" . $row['status'] . "</p>";
+//        }
+
+
+
+        mysqli_query($connection, $sqli2);
+        if (mysqli_query($connection, $sqli2)) {
+            //echo 'check your email';
             $email = $_GET['email'];
-            $to = $email; 
-            $subject= "Email Verification"; 
+            $to = $email;
+            $subject = "Email Verification";
             $message = 'Congratulation! Your charity has been Added successfully';
             //need to use such an admin email here!
             $headers = "From: awongp35@gmail.com \r\n ";
             $headers .= "MIME-Version: 1.0" . "\r\n";
             $headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
-            
-            mail($to, $subject, $message, $headers );
-        
+
+            mail($to, $subject, $message, $headers);
+            //echo 'aaaaaaTRUE';
         } else {
-            echo "Error inserting record: ".mysqli_error($connection);
-//            echo 'aaaaaa';
-        }
-        
-        
-        ?>
-           
-                <?php
-            }
+            echo "Error inserting record: " . mysqli_error($connection);
+            echo 'aaaaaa';
         }
         ?>
+
+        <?php
+    }
+}
+?>
