14ceeb588579e44b24cd028eb829dd1d3c816aeb
LENOVO <LENOVO@192.168.1.12>
2021-10-26 05:42:22 +0300

    Merge origin/master

---
+++ confirmationPage.php
@@ -0,0 +1,36 @@
+
+<html lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel='stylesheet' href='style.css'>
+    <script src="webPro.js"></script>
+    <header id="headerPage" style="padding:28px 16px">
+            <form id="signout" action="logout.php" method="POST">
+                <input type="submit" value="تسجيل خروج">
+            </form> 
+            <img src="logo.jpg" alt="logo" class="pageP"  >
+        </header>
+    <body>
+        
+        <div class="auth-content"> 
+            <hr><br><br>
+           
+            <h1> تم طلب الإنضمام بنجاح  </h1>  
+            
+            <hr><br><br>   
+     </div>
+
+
+   
+                </body>
+                <br><br>
+                <footer class="footer">  
+                    <div class="SOCIAL">
+                        <br>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                    </div>
+                    <p>&copy; KSU|Desigend by Aljawharah, Lamya, Rahaf, Sahar and Leen</p>
+                </footer>
