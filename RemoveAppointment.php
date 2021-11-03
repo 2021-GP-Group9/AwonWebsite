82d6b3687ecca21e0543fc510bc13951c2bbd256
sahar <sahar@192.168.7.59>
2021-11-03 11:33:46 +0300

    manage requests code modify

--- RemoveAppointment.php
+++ RemoveAppointment.php
@@ -4,7 +4,7 @@ session_start();
 require('db_connecting.php');
 
 $charityID = $_SESSION['ID'];
-$appointment_id = $_GET['appointment_id'];
+$appointment_id = $_POST['appointment_id'];
 
 $sql = "DELETE FROM appointment  WHERE id ='$appointment_id' ";
 $result = $conn->query($sql);
