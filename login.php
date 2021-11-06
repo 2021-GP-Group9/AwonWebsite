b2c2f567fa972137d917272aad42f6a0429c87c2
sahar <sahar@localhost>
2021-11-06 13:24:27 +0300

    filter code modification + login copy

--- login.php
+++ login.php
@@ -1,14 +1,13 @@
-
 <?php
 session_start();
-////var_dump(password_hash("12345A", PASSWORD_DEFAULT));
 
+// if the admin loggedin
 if (isset($_SESSION['role'])) {
     if ($_SESSION['role'] == 'admin') {
         header('Location:joiningRequests.php');
     }
 }
-
+//// if the Charity loggedin
 if (isset($_SESSION['role'])) {
     if ($_SESSION['role'] == 'charity') {
         header('Location:charityHome.php');
@@ -28,21 +27,17 @@ if (isset($_SESSION['role'])) {
             <div class="dtr-responsive-header">
                 <div class="container"> 
                     <div class="dtr-header-left" style="float: left;"> 
-                             
+
                     </div>
-                    <!-- small devices logo --> 
+
                     <a href="index.php"><img src="finalLogo.jpeg"  class="m-logo" alt="logo"></a> 
-                    <!-- small devices logo ends --> 
+
                 </div>
             </div>
             <header id="dtr-header-global" class="">
                 <div class="container">
                     <div class="row">
-                        <div class="col-sm-4">
-<!--                            <form id="signout" action="logout.php" method="POST">
-                                <input type="submit" class="logoutbtn" value="تسجيل خروج">
-                            </form>  -->
-                        </div>
+
                         <div class="col-sm-4"></div>
                         <div class="col-sm-4" align="right">
                             <div class="dtr-header-right"> 
@@ -51,13 +46,7 @@ if (isset($_SESSION['role'])) {
                     </div>
                 </div>
             </header>
-            <!-- Header
-            
-            <header id="headerPage" style="padding:28px 16px">
-               <img src="logo.jpg" alt="logo" class="pageP"  >
-        
-            </header>
-            -->
+
 
             <div  id="dtr-main-content"> 
 
@@ -97,15 +86,16 @@ if (isset($_SESSION['role'])) {
                                         <br>
                                         <p>جمعية جديدة؟ <a href="RequestToJoin.php">تسجيل جديد</a></p>
                                         <?php
-                                        //if (isset($_SESSION['errorC'])) {
-                                       //     echo "<span style='color:red'>" . $_SESSION['errorC'] . "</span>";
-                                        //}
-                                       // $_SESSION['errorC'] = null;
+                                        
+                                        // to show Error messages
+                                        if (isset($_SESSION['errorC'])) {
+                                           echo "<span style='color:red'>" . $_SESSION['errorC'] . "</span>";
+                                        }
+                                        $_SESSION['errorC'] = null;
                                         ?>
 
 
                                         <?php
-                                        // put your code here
                                         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                             require('db_connecting.php');
                                             $username = $_POST['username'];
@@ -113,67 +103,56 @@ if (isset($_SESSION['role'])) {
                                             $sql = "select * from admin where username = '$username' ";
                                             $result = $conn->query($sql);
                                             if ($result->num_rows > 0) {
+                                                // if admin want to log in
                                                 $row = mysqli_fetch_assoc($result);
-                                                /// echo "<h1>"."useeeeer is".$row["username"]."</h1>";
-                                                //if(password_verify($password, $row['password'])){
+
                                                 if (password_verify($password, $row['password'])) {
+                                                    //If login for the admin is successful ، password and usrname is correct
                                                     $_SESSION['ID'] = $row['ID'];
                                                     $_SESSION['role'] = 'admin';
 
                                                     echo '<META HTTP-EQUIV="Refresh" Content="0; URL=joiningRequests.php">';
                                                 } else {
+
+                                                    //if password for admin not correct 
+                                                    $_SESSION['errorC'] = 'اسم المستخدم أو كلمة المرور غير صحيحة';
+
                                                     echo "<span style='color:red'>" .'اسم المستخدم أو كلمة المرور غير صحيحة'. "</span>"; 
+
                                                     echo '<META HTTP-EQUIV="Refresh" Content="2; URL=login.php">';
                                                 }
                                             } else {
-                                                $_SESSION['errorC'] = 'اسم المستخدم أو كلمة المرور غير صحيحة';
-                                                echo '<META HTTP-EQUIV="Refresh" Content="2; URL=login.php">';
-                                            }
-                                        }
-                                        ?>
-                                        <!-- charity page code -->
-                                        <?php
-                                        // put your code here
-                                        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                                            require('db_connecting.php');
-                                            $username = $_POST['username'];
-                                            $password = $_POST['pwd'];
+                                                ?>
+                                                <!-- login for charity -->
+                                                <?php
+                                                 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                                                require('db_connecting.php');
+                                                $username = $_POST['username'];
+                                                $password = $_POST['pwd'];
 
-//        $sqli = "SELECT * FROM `charity` WHERE ID='$id' ";
-//       $result = $connection->query($sqli);
-// while ($row = $result->fetch_assoc()) {
-//
-//                         if($id=$row['id'] and $status !='Accepted'){
-//                             $_SESSION['errorC'] = 'the charity not Accepted yet';
-//			header('Location:login.php');
-//                         }
-//        
-// }
+                                                $sql_charity = "select * from charity where username = '$username' and `status`='Accepted'";
+                                                $result = $conn->query($sql_charity);
+                                                if ($result->num_rows > 0) {
+                                                    $row = mysqli_fetch_assoc($result);
 
-                                            $sql_charity = "select * from charity where username = '$username' and `status`='Accepted'";
-                                            $result = $conn->query($sql_charity);
-                                            if ($result->num_rows > 0) {
-                                                $row = mysqli_fetch_assoc($result);
 
-                                                /// echo "<h1>"."useeeeer is".$row["username"]."</h1>";
-//		if ($status !='Accepted'){
-//                             $_SESSION['errorC'] = 'the charity not Accepted yet';
-//			header('Location:login.php');
-//                         }
-                                                //if(password_verify($password, $row['password'])){
-                                                if (password_verify($password, $row['pass'])) {
+                                                    if (password_verify($password, $row['pass'])) {
+                                                        //If login is successful ، password and usrname is correct 
 
-                                                    $_SESSION['ID'] = $row['ID'];
-                                                    $_SESSION['role'] = 'charity';
-                                                    echo '<META HTTP-EQUIV="Refresh" Content="2; URL=charityHome.php">';
-//                        
+                                                        $_SESSION['ID'] = $row['ID'];
+                                                        $_SESSION['role'] = 'charity';
+                                                        echo '<META HTTP-EQUIV="Refresh" Content="2; URL=charityHome.php">';
+                                                        //if password not correct 
+                                                    } else {
+                                                        $_SESSION['errorC'] = 'اسم المستخدم أو كلمة المرور غير صحيحة ';
+                                                        echo '<META HTTP-EQUIV="Refresh" Content="2; URL=login.php">';
+                                                    }
+                                                    // If the username does not exist or the charity has not yet been accepted
                                                 } else {
-                                                    $_SESSION['errorC'] = 'اسم المستخدم أو كلمة المرور غير صحيحة أو لم تقبل الجمعية بعد';
+                                                    $_SESSION['errorC'] = 'اسم المستخدم  غير صحيح أو الجمعية لم تقبل بعد  ';
                                                     echo '<META HTTP-EQUIV="Refresh" Content="2; URL=login.php">';
                                                 }
-                                            } else {
-                                                $_SESSION['errorC'] = 'اسم المستخدم أو كلمة المرور غير صحيحة أو لم تقبل الجمعية بعد';
-                                                echo '<META HTTP-EQUIV="Refresh" Content="2; URL=login.php">';
+                                            }
                                             }
                                         }
                                         ?>
@@ -184,102 +163,10 @@ if (isset($_SESSION['role'])) {
                             </div>
                             <!-- column 2 ends --> 
                         </div>
-                        <!--===== row 1 ends =====--> 
+                        <!-- row 1 ends --> 
                     </div>
                 </section>
-                <!-- ----------------------------------------------------------------------------------------
-                <div class="auth-content"> 
-                <!-- design 
-                <form id="loginForm" method="post" > 
-                    <h2> <br>تسجيل الدخول</h2>
-                    <br>
-                    <br>
-                    <label class="name"><h3>   <input type="text" name="username" id="username" class="name-input" required>اسم المستخدم </label> </h3><br><br>
-                        <br>
-                        <label class="name"><h3>   <input type="password" name="pwd" class="password" id="password">كلمة المرور </label></h3>
-                            <br>
-                            <input type="submit" class="bu1" value="تسجيل الدخول"/>
-                            <br><br>
-                            <p>جمعية جديدة? <a href ="RequestToJoin.php" > تسجيل جديد </a></p> --> 
 
-                <?php
-//                                if (isset($_SESSION['errorC'])) {
-//                                    echo "<span style='color:red'>" . $_SESSION['errorC'] . "</span>";
-//                                }
-//                                $_SESSION['errorC'] = null;
-                ?>
-                <?php
-                // put your code here
-//                                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//                                    require('db_connecting.php');
-//                                    $username = $_POST['username'];
-//                                    $password = $_POST['pwd'];
-//                                    $sql = "select * from admin where username = '$username' ";
-//                                    $result = $conn->query($sql);
-//                                    if ($result->num_rows > 0) {
-//                                        $row = mysqli_fetch_assoc($result);
-//                                        /// echo "<h1>"."useeeeer is".$row["username"]."</h1>";
-//                                        //if(password_verify($password, $row['password'])){
-//                                        if (password_verify($password, $row['password'])) {
-//                                            $_SESSION['ID'] = $row['ID'];
-//                                            $_SESSION['role'] = 'admin';
-//
-//                                            echo '<META HTTP-EQUIV="Refresh" Content="1; URL=joiningRequests.php">';
-//                                        } else {
-//                                            $_SESSION['errorC'] = 'UserName or password is not correct';
-//                                            echo '<META HTTP-EQUIV="Refresh" Content="2; URL=login.php">';
-//                                        }
-//                                    } else {
-//                                        $_SESSION['errorC'] = 'UserName or password is not correct';
-//                                        echo '<META HTTP-EQUIV="Refresh" Content="2; URL=login.php">';
-//                                    }
-//                                }
-                ?>
-                <!-- charity page code -->
-                <?php
-                // put your code here
-//                                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//                                    require('db_connecting.php');
-//                                    $username = $_POST['username'];
-//                                    $password = $_POST['pwd'];
-//        $sqli = "SELECT * FROM `charity` WHERE ID='$id' ";
-//       $result = $connection->query($sqli);
-// while ($row = $result->fetch_assoc()) {
-//
-//                         if($id=$row['id'] and $status !='Accepted'){
-//                             $_SESSION['errorC'] = 'the charity not Accepted yet';
-//			header('Location:login.php');
-//                         }
-//        
-// }
-//
-//                                    $sql_charity = "select * from charity where username = '$username' and `status`='Accepted'";
-//                                    $result = $conn->query($sql_charity);
-//                                    if ($result->num_rows > 0) {
-//                                        $row = mysqli_fetch_assoc($result);
-//
-//                                        /// echo "<h1>"."useeeeer is".$row["username"]."</h1>";
-////		if ($status !='Accepted'){
-////                             $_SESSION['errorC'] = 'the charity not Accepted yet';
-////			header('Location:login.php');
-////                         }
-//                                        //if(password_verify($password, $row['password'])){
-//                                        if (password_verify($password, $row['pass'])) {
-//
-//                                            $_SESSION['ID'] = $row['ID'];
-//                                            $_SESSION['role'] = 'charity';
-//                                            echo '<META HTTP-EQUIV="Refresh" Content="2; URL=CharityPage.php">';
-////                        
-//                                        } else {
-//                                            $_SESSION['errorC'] = 'UserName or password is not correct';
-//                                            echo '<META HTTP-EQUIV="Refresh" Content="2; URL=login.php">';
-//                                        }
-//                                    } else {
-//                                        $_SESSION['errorC'] = 'Username not currect or the charity not accepted yet';
-//                                        echo '<META HTTP-EQUIV="Refresh" Content="2; URL=login.php">';
-//                                    }
-//                                }
-                ?>
                 <footer id="dtr-footer"> 
 
                     <!--== copyright starts ==-->
@@ -292,11 +179,11 @@ if (isset($_SESSION['role'])) {
                                     <p>&copy; فريق منصة عون</p>
                                 </div>
                             </div>
-                            <!--== row ends ==--> 
+                            <!-- row ends --> 
 
                         </div>
                     </div>
-                    <!--== copyright ends ==--> 
+                    <!-- copyright ends --> 
 
                 </footer>
                 <!-- footer section ends
@@ -307,12 +194,6 @@ if (isset($_SESSION['role'])) {
 
         </div>
     </body>
-<script src="design.js"></script> 
-    <!-- Footer 
-    <footer>
-        <!-- we want footer with  <p>&copy; فريق منصة عون</p>  
 
-        <p>&copy; فريق منصة عون</p>
-    </footer>-->
+
 </html>
-
