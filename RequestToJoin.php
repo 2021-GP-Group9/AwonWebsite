0f9e20c11322a26867ef6a54227771ebe7ae594a
LENOVO <LENOVO@192.168.1.73>
2021-11-29 15:30:43 +0300

    Merge origin/master

--- RequestToJoin.php
+++ RequestToJoin.php
@@ -66,7 +66,7 @@ if (isset($_SESSION['role'])) {
                                                 }
                                                 $_SESSION['faild'] = null;
                                                 ?>
-                                                <form id="contactform" method="POST"> 
+                                                <form id="contactform" method="POST" action="upload.php" enctype="multipart/form-data"> 
                                                     <fieldset>
                                                         <div class="dtr-form-row dtr-form-row-2col">
                                                             <p class="dtr-form-column">
